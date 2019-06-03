1e1a9d3ca347f581fa970ff3687c3713d7184bbf
= <=>
2019-06-03 14:04:54 +0700

    intentNC2

---
+++ app/src/main/java/com/example/intentnc2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.intentnc2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showWebPage(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.youtube.com"));
+        startActivity(intent);
+    }
+}
